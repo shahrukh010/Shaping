d26bbad542375bcae9a71eac37ca8c7243b28e4a
shahrukh010 <khanshahrukhbsc@gmail.com>
2022-11-30 16:05:30 +0530

    Shaping architecture only

---
+++ ShapingWeb/ShapingBack/src/main/java/com/shaping/code/admin/MainController.java
@@ -0,0 +1,17 @@
+package com.shaping.code.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String viewPage() {
+
+		return "hello shaping";
+	}
+
+}
---
+++ ShapingWeb/ShapingBack/src/main/java/com/shaping/code/admin/user/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.shaping.code.admin.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shaping.entity.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+}
