9792d113ae4e9e7da59222b14b5fbb3353936697
shahrukh010 <khanshahrukhbsc@gmail.com>
2022-11-30 16:43:34 +0530

    working on user entity

--- ShapingEntity/src/main/java/com/shaping/entity/User.java
+++ ShapingEntity/src/main/java/com/shaping/entity/User.java
@@ -2,6 +2,8 @@ package com.shaping.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class User {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 
 	@Column(name = "first_name", length = 30, nullable = false)
@@ -20,10 +23,65 @@ public class User {
 
 	@Column(nullable = false)
 	private String email;
-	
-	@Column(length=100)
+
+	@Column(length = 100)
 	private String password;
 
-
 	private String image;
+
+	public User() {
+	}
+
+	public User(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
 }
