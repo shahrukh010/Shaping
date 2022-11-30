4dc4b0ae98e3abb934f22f34d88b519e84d0fa16
shahrukh010 <khanshahrukhbsc@gmail.com>
2022-11-30 16:40:25 +0530

    working on user entity

--- ShapingEntity/src/main/java/com/shaping/entity/User.java
+++ ShapingEntity/src/main/java/com/shaping/entity/User.java
@@ -1,10 +1,29 @@
 package com.shaping.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
 
+	@Id
+	private long id;
+
+	@Column(name = "first_name", length = 30, nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", length = 30, nullable = false)
+	private String lastName;
+
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(length=100)
+	private String password;
+
+
+	private String image;
 }
