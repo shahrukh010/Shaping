d7706be192eb2a3bb83836ce51c1e82222b1e6ba
shahrukh010 <khanshahrukhbsc@gmail.com>
2022-12-01 07:53:23 +0530

    fixed issue using hashcode(),equals() method

--- ShapingWeb/ShapingBack/src/test/java/com/shaping/code/admin/repository/RoleRepositoryTest.java
+++ ShapingWeb/ShapingBack/src/test/java/com/shaping/code/admin/repository/RoleRepositoryTest.java
@@ -1,6 +1,4 @@
-package com.shaping.code.admin.user;
-
-import java.util.List;
+package com.shaping.code.admin.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +7,40 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import com.shaping.code.admin.user.RoleRepository;
 import com.shaping.entity.Role;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace=Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class RoleRepositoryTest {
-	
+
 	@Autowired
 	private RoleRepository role;
-	
+
 	@Test
 	public void createRole() {
-		
-		Role admin = new Role(1l,"Admin","Manage Administration");
-		Role vendor = new Role(2l,"Vendor","Manage Product");
-		Role editor = new Role(3l,"Editor","Manage Product and Shiping");
-		Role courier = new Role(4l,"Courier","Manage Product Track and Delivery");
-		
-		Role saveAdmin = role.save(admin);
-		Role saveVendor = role.save(vendor);
-		Role saveEditor = role.save(editor);
-		Role saveCourier = role.save(courier);
-		
-		role.saveAll(List.of(saveAdmin,saveVendor,saveEditor,saveCourier));
-		
+
+		/*
+		 * Role admin = new Role(1l,"Admin","Manage Administration"); Role vendor = new
+		 * Role(2l,"Vendor","Manage Product"); Role editor = new
+		 * Role(3l,"Editor","Manage Product and Shiping"); Role courier = new
+		 * Role(4l,"Courier","Manage Product Track and Delivery");
+		 * 
+		 * Role saveAdmin = role.save(admin); Role saveVendor = role.save(vendor); Role
+		 * saveEditor = role.save(editor); Role saveCourier = role.save(courier);
+		 * 
+		 * role.saveAll(List.of(saveAdmin,saveVendor,saveEditor,saveCourier));
+		 */
+
+		Role role = new Role();
+		role.setId(5l);
+		role.setName("Assistance");
+		role.setDescription("Manage product review and feedback");
+		this.role.save(role);
+
 	}
+	
+	
 
 }
--- ShapingWeb/ShapingBack/src/test/java/com/shaping/code/admin/repository/UserRepositoryTest.java
+++ ShapingWeb/ShapingBack/src/test/java/com/shaping/code/admin/repository/UserRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.shaping.code.admin.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -18,18 +24,58 @@ public class UserRepositoryTest {
 	@Autowired
 	private UserRepository user;
 
+	@Autowired
+	private EntityManager entityManager;
+
 	@Test
 	public void createUser() {
 
-		Role role = new Role(1l);
+		Role role = entityManager.find(Role.class, 2l);
 		User user = new User();
-		user.setFirstName("shahrukh");
-		user.setLastName("khan");
-		user.setEmail("khanshahrukhbsc@gmail.com");
+		user.setFirstName("Bridget");
+		user.setLastName("Smith");
+		user.setEmail("bridgetsmith@gmail.com");
 		user.setPassword("annieHector");
 		user.setEnabled(false);
 		user.addRole(role);
-		this.user.save(user);
+		User saveUser = this.user.save(user);
+		assertThat(saveUser.getId()).isGreaterThan(0);
 
 	}
+
+	@Test
+	public void createUserWithMoreRole() {
+
+		Role asistance = entityManager.find(Role.class, 5l);
+		Role courier = entityManager.find(Role.class, 4l);
+		Role vendor = new Role(2l);
+		Role admin = new Role(1l);
+		Role admin2 = new Role(1l);
+
+		User user = new User();
+		user.setFirstName("Juli");
+		user.setLastName("Smith");
+		user.setEmail("julismith@gmail.com");
+		user.setPassword("annieHector");
+		user.setEnabled(true);
+//		user.addRole(asistance);
+//		user.addRole(admin);//here you can see error will not raise because of same reference will store here admin
+//		user.addRole(admin);
+		
+		user.addRole(asistance);
+		user.addRole(admin);
+		user.addRole(admin2);//in this case error will be raise because of different object are there so handle this kind of case use hascode(),equals()
+		User saveUser = this.user.save(user);
+		assertThat(saveUser.getId()).isGreaterThan(0);
+
+	}
+
+	@Test
+	public void listAllUsers() {
+
+		List<User> users = (List<User>) user.findAll();
+
+		users.forEach(usr -> System.out.println(usr.getFirstName()));
+	}
+
 }
